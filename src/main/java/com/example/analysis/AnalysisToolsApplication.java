a066cdc3917322a3561b8548c73ca4152625ea1b
hxtonge <1286848541@qq.com>
2022-08-19 18:54:50 +0800

    Initial commit

---
+++ src/main/java/com/example/analysis/AnalysisToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalysisToolsApplication.class, args);
+    }
+
+}
